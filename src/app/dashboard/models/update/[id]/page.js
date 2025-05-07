8eaa5fe77e57c43ce812fc240ba29880e5ef06cb
Artem <101283788+WhoAmI471@users.noreply.github.com>
2025-05-07 15:47:20 +0500

    Returns upload & update model.

--- src/app/dashboard/models/update/[id]/page.js
+++ src/app/dashboard/models/update/[id]/page.js
@@ -1,14 +1,14 @@
 'use client'
 import ModelEditForm from '@/components/ModelEditForm'
-
+import { use } from 'react';
 
 export default function UpdatePage({ params }) {
-  const { id } = params
-
+  const { id } = use(params)
+  // console.log(params.id);
   return (
     <div className="p-6 max-w-3xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Редактирование модели</h1>
-      <ModelEditForm modelId={id} />
+      <ModelEditForm id={ id } />
     </div>
   )
 }
