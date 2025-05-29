40f3e79116e0ba61638e492096ce31b78f0b5ffc
Artem <ga@digi-tech.dev>
2025-05-29 08:31:06 +0500

    Добавляет иконки на верхнюю панель.

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,6 +1,9 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import Logo from "../../public/Logo.svg"
 import axios from 'axios'
 
 export default function MainHeader() {
@@ -28,15 +31,22 @@ export default function MainHeader() {
 
   return (
     <header className="flex justify-between items-center p-4 bg-blue-600 text-white shadow">
-      <div className="text-xl font-bold">DigiTech</div>
+      <div className="text-xl font-bold">
+        <Image 
+          src={Logo} 
+          alt="DigiTech Logo" 
+          width={135} 
+          height={128}
+        />
+      </div>
       {user ? (
         <div className="flex items-center gap-4">
           <span>{user.name}</span>
           <button
             onClick={handleLogout}
-            className="bg-white text-blue-600 px-3 py-1 rounded hover:bg-gray-100"
+            // className="bg-white text-blue-600 px-3 py-1 rounded hover:bg-gray-100"
           >
-            Выйти
+            <ArrowRightStartOnRectangleIcon className="h-8 w-8" />
           </button>
         </div>
       ) : (
